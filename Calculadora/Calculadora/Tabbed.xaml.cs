190d06bc72326fb15d8e3a80da6743981e5bbc76
victor7395 <victor_fernandes1995@hotmail.com>
2017-07-21 18:57:15 -0300

    Initial commit

---
+++ Calculadora/Calculadora/Tabbed.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculadora
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tabbed : TabbedPage
+    {
+        public Tabbed ()
+        {
+            InitializeComponent();
+            
+
+            var carb = new NavigationPage(new Carboidratos());
+            carb.Title = "Carboidratos";
+            Children.Add(carb);
+
+            var carne = new NavigationPage(new Carnes());
+            carne.Title = "Carnes";
+            Children.Add(carne);
+        }
+    }
+}
